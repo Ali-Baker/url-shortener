ef20ad33cc6dcc5eb5b17f2181a637403aa74e62
Ali <alialbaker2007@gmail.com>
2020-11-28 22:56:02 +0000

    Add input group

---
+++ packages/frontend/src/components/InputGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" />
+    <button @click.prevent="shorten">Shorten</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    shorten() {
+      console.log('hello');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
