386512209e7b7e83fcc9ef914e24878f6a1e4629
Ali <alialbaker2007@gmail.com>
2020-11-29 14:32:02 +0000

    Add input group style

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -23,12 +23,18 @@ export default defineComponent({
 </script>
 
 <style>
+body {
+  width: 100wh;
+  display: flex;
+  justify-content: center;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+  max-width: 1200px;
+  width: 100%;
 }
 </style>
--- packages/frontend/src/components/InputGroup.vue
+++ packages/frontend/src/components/InputGroup.vue
@@ -27,5 +27,38 @@ export default {
 };
 </script>
 
-<style>
+<style scopped>
+form {
+  position: relative;
+}
+
+input {
+  border: 4px #FFBC42 solid;
+  border-radius: 5px 0 0 5px;
+  width: 70%;
+  height: 26px;
+  padding: 8px;
+  outline: none;
+}
+
+button {
+  border: 4px #FFBC42 solid;
+  border-radius: 0 5px 5px 0;
+  background-color: #FFBC42;
+  color: black;
+  height: 50px;
+  width: 20%;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+button:hover {
+  color: white;
+}
 </style>
