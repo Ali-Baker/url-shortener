573d45ef601a346bbdf64cda95c34557e000881f
Ali <alialbaker2007@gmail.com>
2020-11-29 10:21:12 +0000

    Add route

--- packages/backend/routes/index.js
+++ packages/backend/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const crc32 = require('crc32')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const ShortUrl = require('../models/ShortUrl');
+
+const shortUrlValidator = async (req, res, next) => {
+  if(!req.body.fullUrl) {
+    res.send({
+      message: 'fullUrl is required!'
+    }).status(400);
+  } else {
+    const shortUrlId = crc32(req.body.fullUrl);
+    const shortUrl = await ShortUrl.findOne({ _id: shortUrlId });
+    if (shortUrl) {
+      res.json(shortUrl)
+    } else {
+      next();
+    }
+  }
+}
+
+router.post('/shortUrls', shortUrlValidator,  async (req, res) => {
+  const shortUrl = crc32(req.body.fullUrl);
+  const url = await ShortUrl.create({ 
+    full: req.body.fullUrl,
+    short: shortUrl,
+    _id: shortUrl
+  });
+
+  res.json(url);
 });
 
 module.exports = router;
