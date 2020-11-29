be6aa614579f5a46d38b3cca400bfd645cfc2f99
Ali <alialbaker2007@gmail.com>
2020-11-29 11:07:09 +0000

    Add api config

---
+++ packages/frontend/src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000';
+
+const Api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+export const all = () => Api.get('/all');
+
+export default {};
