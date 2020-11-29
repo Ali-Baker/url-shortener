a11879ca667a59f3c3c41426e6147b0f62245f52
Ali <alialbaker2007@gmail.com>
2020-11-29 11:05:42 +0000

    Add cors

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,11 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 
