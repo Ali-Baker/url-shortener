f5e8e4369e06d91e1b23f082582c1af4d8890d92
Ali <alialbaker2007@gmail.com>
2020-11-29 11:06:42 +0000

    Add UrlsList component

---
+++ packages/frontend/src/components/UrlsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li v-for="url in urls" :key="url._id">
+        {{url.short}}
+      </li>
+  </ul>
+</template>
+
+<script>
+import { all } from '../api';
+
+export default {
+  name: 'UrlsList',
+  data() {
+    return {
+      urls: [],
+      loading: false,
+      errorMessage: '',
+    };
+  },
+  created() {
+    this.loading = true;
+    all().then((responce) => {
+      this.loading = false;
+      this.urls = responce.data;
+    }).catch((error) => {
+      this.loading = false;
+      this.errorMessage = error.message;
+    });
+  },
+};
+</script>
+
+<style>
+
+</style>
